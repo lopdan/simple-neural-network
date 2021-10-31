4ef4e45cffc54c4fa971d7981605724cfb1c87c5
TheCraxo <62883984+TheCraxo@users.noreply.github.com>
2021-10-31 18:26:25 +0100

    Neural network training

--- src/main.py
+++ src/main.py
@@ -2,10 +2,26 @@ import numpy as np
 from neural_network import NeuralNetwork
 
 
+data = np.array([
+  [-2, -1],
+  [25, 6], 
+  [17, 4], 
+  [-15, -6],
+  [28, 5],
+  [-4, -3]
+])
+all_y_trues = np.array([
+  1,
+  0,
+  0, 
+  1,
+  0,
+  1, 
+])
+
 def main():
 	network = NeuralNetwork()
-	x = np.array([2, 3])
-	print(network.feedforward(x))
+	network.train(data, all_y_trues)
  
 if __name__ == "__main__":
 	main()
--- src/neuron.py
+++ src/neuron.py
@@ -1,9 +1,5 @@
 import numpy as np
 
-def sigmoid(x):
-  # f(x) = 1 / (1 + e^(-x))
-  return 1 / (1 + np.exp(-x))
-
 class Neuron:
   """Neuron constructor"""
   def __init__(self, weights, bias):
@@ -12,9 +8,10 @@ class Neuron:
 
   """Calculates the output of the neuron
   Returns:
-		int: output from neuron
+    int: output from neuron
   """
   def feedforward(self, inputs):
     # Weight inputs, add bias, then use the activation function
     total = np.dot(self.weights, inputs) + self.bias
-    return sigmoid(total)
+    return self.sigmoid(total)
+ 
