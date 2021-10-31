9f3c3bd4c5ab067eb28995f3cb740e5c67d3bb1a
TheCraxo <62883984+TheCraxo@users.noreply.github.com>
2021-10-31 18:34:49 +0100

    Names refactor

--- src/neural_network.py
+++ src/neural_network.py
@@ -1,7 +1,5 @@
 import numpy as np
 
-from neuron import Neuron
-
 def sigmoid(x):
   # f(x) = 1 / (1 + e^(-x))
   return 1 / (1 + np.exp(-x))
@@ -51,17 +49,17 @@ class NeuralNetwork:
     return o1
 
   def train(self, data, all_y_trues):
-    '''
+    """
     - data is a (n x 2) numpy array, n = # of samples in the dataset.
     - all_y_trues is a numpy array with n elements.
       Elements in all_y_trues correspond to those in data.
-    '''
-    learn_rate = 0.1
-    epochs = 1000 # number of times to loop through the entire dataset
+    """
+    learn_rate = 0.05
+    epochs = 2000 # number of times to loop through the entire dataset
 
     for epoch in range(epochs):
       for x, y_true in zip(data, all_y_trues):
-        # --- Do a feedforward (we'll need these values later)
+        # Do a feedforward (we'll need these values later)
         sum_h1 = self.w1 * x[0] + self.w2 * x[1] + self.b1
         h1 = sigmoid(sum_h1)
 
@@ -72,45 +70,44 @@ class NeuralNetwork:
         o1 = sigmoid(sum_o1)
         y_pred = o1
 
-        # --- Calculate partial derivatives.
-        # --- Naming: d_L_d_w1 represents "partial L / partial w1"
-        d_L_d_ypred = -2 * (y_true - y_pred)
+        # Calculate partial derivatives.
+        # Naming: d_L_d_w1 represents "partial L / partial w1"
+        derivative_Lypred = -2 * (y_true - y_pred)
 
         # Neuron o1
-        d_ypred_d_w5 = h1 * deriv_sigmoid(sum_o1)
-        d_ypred_d_w6 = h2 * deriv_sigmoid(sum_o1)
-        d_ypred_d_b3 = deriv_sigmoid(sum_o1)
-
-        d_ypred_d_h1 = self.w5 * deriv_sigmoid(sum_o1)
-        d_ypred_d_h2 = self.w6 * deriv_sigmoid(sum_o1)
+        derivative_o1w5 = h1 * deriv_sigmoid(sum_o1)
+        derivative_o1w6 = h2 * deriv_sigmoid(sum_o1)
+        derivative_o1b3 = deriv_sigmoid(sum_o1)
+        derivative_o1h1 = self.w5 * deriv_sigmoid(sum_o1)
+        derivative_o1h2 = self.w6 * deriv_sigmoid(sum_o1)
 
         # Neuron h1
-        d_h1_d_w1 = x[0] * deriv_sigmoid(sum_h1)
-        d_h1_d_w2 = x[1] * deriv_sigmoid(sum_h1)
-        d_h1_d_b1 = deriv_sigmoid(sum_h1)
+        derivative_h1w1 = x[0] * deriv_sigmoid(sum_h1)
+        derivative_h1w2 = x[1] * deriv_sigmoid(sum_h1)
+        derivative_h1b1 = deriv_sigmoid(sum_h1)
 
         # Neuron h2
-        d_h2_d_w3 = x[0] * deriv_sigmoid(sum_h2)
-        d_h2_d_w4 = x[1] * deriv_sigmoid(sum_h2)
-        d_h2_d_b2 = deriv_sigmoid(sum_h2)
+        derivative_h2w3 = x[0] * deriv_sigmoid(sum_h2)
+        derivative_h2w4 = x[1] * deriv_sigmoid(sum_h2)
+        derivative_h2b2 = deriv_sigmoid(sum_h2)
 
-        # --- Update weights and biases
+        # Update weights and bias
         # Neuron h1
-        self.w1 -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_w1
-        self.w2 -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_w2
-        self.b1 -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_b1
+        self.w1 -= learn_rate * derivative_Lypred * derivative_o1h1 * derivative_h1w1
+        self.w2 -= learn_rate * derivative_Lypred * derivative_o1h1 * derivative_h1w2
+        self.b1 -= learn_rate * derivative_Lypred * derivative_o1h1 * derivative_h1b1
 
         # Neuron h2
-        self.w3 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_w3
-        self.w4 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_w4
-        self.b2 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_b2
+        self.w3 -= learn_rate * derivative_Lypred * derivative_o1h2 * derivative_h2w3
+        self.w4 -= learn_rate * derivative_Lypred * derivative_o1h2 * derivative_h2w4
+        self.b2 -= learn_rate * derivative_Lypred * derivative_o1h2 * derivative_h2b2
 
         # Neuron o1
-        self.w5 -= learn_rate * d_L_d_ypred * d_ypred_d_w5
-        self.w6 -= learn_rate * d_L_d_ypred * d_ypred_d_w6
-        self.b3 -= learn_rate * d_L_d_ypred * d_ypred_d_b3
+        self.w5 -= learn_rate * derivative_Lypred * derivative_o1w5
+        self.w6 -= learn_rate * derivative_Lypred * derivative_o1w6
+        self.b3 -= learn_rate * derivative_Lypred * derivative_o1b3
 
-      # --- Calculate total loss at the end of each epoch
+      # Calculate loss
       if epoch % 10 == 0:
         y_preds = np.apply_along_axis(self.feedforward, 1, data)
         loss = mse_loss(all_y_trues, y_preds)
